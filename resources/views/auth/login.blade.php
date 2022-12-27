f3c0d94adaa1d7a2e64523df0a0527cb1cacc66c
lupete3 <lupeteplacide@gmail.com>
2022-12-27 10:30:15 +0100

    Add layout template to dashboard page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-guest-layout >
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -33,6 +33,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
